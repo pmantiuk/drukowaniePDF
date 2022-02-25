7f68a505c823a04ddf83dd607cfca4c4184727fc
Paweł <74363551+pmantiuk@users.noreply.github.com>
2022-02-25 13:45:45 +0100

    Add files via upload

--- printek.py
+++ printek.py
@@ -1,43 +1,52 @@
 from PyPDF2 import PdfFileWriter, PdfFileReader
 from pdfCropMargins import crop
-import minecart
+import os
+# import minecart
+rysunek = '267-ML-106X-SD-05.pdf'
 
-# with open("2-003Y-SZ-02.pdf", "rb") as in_f:
-#     input1 = PdfFileReader(in_f, strict = False)
-#     output = PdfFileWriter()
+path = os.listdir(r'C:\Users\pmantiuk\Desktop\druki formatówka')
 
-#     numPages = input1.getNumPages()
-#     print (f"document has {numPages} pages.")
+# print(path)
 
-#     wsp_finalne = {
-#              'warstwa -4' : (0, 100, 1750, 550),
-#              'warstwa -3' : (0, 550, 1750, 950), 
-#              'warstwa -2' : (0, 950, 1750, 1350), 
-#              'warstwa -1' : (0, 1300, 1750, 1717), # jest okej wsp_pomocnicze (225, 1310, 1750, 1665)
-#              'warstwa  1' : (0, 1725, 1750, 2130), # jest okej wsp_pomocnicze (225, 1725, 1750, 2080)
-#              'warstwa  2' : (0, 2080, 1750, 2530)  # jest okej wsp_pomocnicze (225, 2150, 1750, 2480)
-#              } 
+with open(rysunek, "rb") as in_f:
+    input1 = PdfFileReader(in_f, strict = False)
+    output = PdfFileWriter()
+
+    numPages = input1.getNumPages()
+    print (f"document has {numPages} pages.")
+
+    wsp_finalne = {
+              'warstwa -4' : (0, 100, 1750, 550),
+              'warstwa -3' : (0, 550, 1750, 950), 
+              'warstwa -2' : (0, 950, 1750, 1350), 
+              'warstwa -1' : (0, 1300, 1750, 1717), # jest okej wsp_pomocnicze (225, 1310, 1750, 1665)
+              'warstwa  1' : (0, 1725, 1750, 2130), # jest okej wsp_pomocnicze (225, 1725, 1750, 2080)
+              'warstwa  2' : (0, 2080, 1750, 2530)  # jest okej wsp_pomocnicze (225, 2150, 1750, 2480)
+              } 
 
 #--- wydruk pomocniczy do określenia prawej krawędzi wydruku oraz upewnienia się, że obszar zawiera ściany
 
-    # for i in range(numPages):
-    #     page = input1.getPage(i)
-    #     print (page.mediaBox.getUpperRight_x(), page.mediaBox.getUpperRight_y())
-    #     # page.trimBox.lowerLeft = (0, 0)
-    #     # page.trimBox.upperRight = (1225, 225)
-    #     page.cropBox.lowerLeft = (225, 2150)
-    #     page.cropBox.upperRight = (1750, 2480)
-    #     output.addPage(page)
+    for i in range(numPages):
+        page = input1.getPage(i)
+        print (page.mediaBox.getLowerLeft(), page.mediaBox.getUpperRight())
 
-    # with open("out.pdf", "wb") as out_f:
-    #     output.write(out_f)
+        print(f'wysokość rysunku: {page.mediaBox.getHeight()}')
+        print(f'szerokość rysunku: {page.mediaBox.getWidth()}')
+        # page.trimBox.lowerLeft = (0, 0)
+        # page.trimBox.upperRight = (1225, 225)
+        page.cropBox.lowerLeft = (1582, 1500)
+        page.cropBox.upperRight = (2384, 3370)
+        output.addPage(page)
+
+    with open("out.pdf", "wb") as out_f:
+        output.write(out_f)
  
 #--- odnajdywanie ilości kolorów w pdfie
-pdffile = open('out.pdf', 'rb')
-doc = minecart.Document(pdffile)
-page = doc.get_page(0)
-for shape in page.shapes.iter_in_bbox((0, 0, 100, 200)):
-     print (shape.path, shape.fill.color.as_rgb())
+# pdffile = open('out.pdf', 'rb')
+# doc = minecart.Document(pdffile)
+# page = doc.get_page(0)
+# for shape in page.shapes.iter_in_bbox((0, 0, 100, 200)):
+#      print (shape.path, shape.fill.color.as_rgb())
      
 # m = page.images[0].as_pil()  # requires pillow
 # im.show()
